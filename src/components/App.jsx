8b782226f062587cb0354c49eea459faf6fd5abf
NataliiaYa <118573896+NataliiaYa@users.noreply.github.com>
2023-07-04 01:00:18 +0300

    fixer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
+export const App = ({ children }) => {
   return (
-    <div
+    <div 
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {children}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        likes={user.stats.likes}
+        views={user.stats.views}
+      
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
